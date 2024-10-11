ae67b7d27efb0502c6e7424762cbf6f35860f3b3
Rachana Kallada Jayaraj <kjrach@gmail.com>
2024-10-10 20:21:45 -0400

    Linking the button to a new activity

--- app/src/main/java/edu/northeastern/a6_group10/MainActivity.java
+++ app/src/main/java/edu/northeastern/a6_group10/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.northeastern.a6_group10;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button atYourServiceButton = findViewById(R.id.at_your_service_button);
+
+        atYourServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AtYourService.class);
+                startActivity(intent);
+            }
+        });
     }
 }
