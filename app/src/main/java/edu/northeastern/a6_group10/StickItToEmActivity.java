c32070bfd9fcf1bb6767c95a9a5f2f5b7e6455df
Rachana Kallada Jayaraj <kjrach@gmail.com>
2024-10-23 20:09:36 -0400

    Stick to to Em opens new activity

--- app/src/main/java/edu/northeastern/a6_group10/StickItToEmActivity.java
+++ app/src/main/java/edu/northeastern/a6_group10/StickItToEmActivity.java
@@ -1,4 +1,13 @@
 package edu.northeastern.a6_group10;
 
-public class StickItToEmActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StickItToEmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stick_it_to_em);
+    }
 }
