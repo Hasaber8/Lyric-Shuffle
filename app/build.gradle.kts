25797a73e028b858a6e8bd57767707631b48d26a
Pavan Rao <pavanpej@gmail.com>
2024-10-25 15:19:47 -0700

    feat: initialize firebase and add testing button to MainActivity (#1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +44,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    /**
+     * Firebase dependencies
+     */
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+    implementation("com.google.firebase:firebase-database")
 }
--- app/src/main/java/edu/northeastern/a6_group10/MainActivity.java
+++ app/src/main/java/edu/northeastern/a6_group10/MainActivity.java
@@ -3,8 +3,14 @@ package edu.northeastern.a6_group10;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setup connection to Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference tempKey = database.getReference("temp");
+
+        Button testFirebaseButton = findViewById(R.id.button_test_firebase);
+
+        testFirebaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "Setting value in Firebase Database");
+                Task<Void> t = tempKey.setValue("Hello, World! " + System.currentTimeMillis());
+                t.addOnSuccessListener(aVoid -> {
+                    Log.d("MainActivity", "Value set in Firebase Database");
+                    Toast.makeText(MainActivity.this, "Value set in Firebase Database", Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
 
         Button atYourServiceButton = findViewById(R.id.at_your_service_button);
 
