1798e4120e3c67ff0a40a536eb6e1e8237b0a779
Rohan Hasabe <rohanhasabe8@gmail.com>
2024-10-25 18:58:03 -0400

    assignment: 7: Add base notification code
    Change-Id: I53c9112a7e1d48a1692f6d90e3d751899a5faffd
    Signed-off-by: Rohan Hasabe <rohanhasabe8@gmail.com>

--- app/src/main/java/edu/northeastern/a6_group10/FeedActivity.java
+++ app/src/main/java/edu/northeastern/a6_group10/FeedActivity.java
@@ -1,18 +1,29 @@
 package edu.northeastern.a6_group10;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
 
+    public static final String STICKER_SERVICE_CHANNEL_ID = "stickItToEm";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        createNotificationChannel();
+        showNotification();
 
         RecyclerView stickerRecyclerView = findViewById(R.id.stickerRecyclerView);
         stickerRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -38,5 +49,31 @@ public class FeedActivity extends AppCompatActivity {
         // Set the adapter
         StickerRecycler adapter = new StickerRecycler(stickerList);
         stickerRecyclerView.setAdapter(adapter);
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    STICKER_SERVICE_CHANNEL_ID,
+                    getString(R.string.stickerChannelName),
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel.setDescription(getString(R.string.stickerChannelDescription));
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, STICKER_SERVICE_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_chat)
+                .setContentTitle(getString(R.string.stickerChannelName))
+                .setContentText(getString(R.string.stickerChannelDescription))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(1, notificationBuilder.build());
     }
 }
