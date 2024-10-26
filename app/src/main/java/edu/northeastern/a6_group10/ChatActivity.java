952c74305b2936f252d0599fe89947d80e692703
Pavan Rao <pavanpej@gmail.com>
2024-10-26 19:02:01 -0400

    feat: Initial chat list integration with Firebase

--- app/src/main/java/edu/northeastern/a6_group10/ChatActivity.java
+++ app/src/main/java/edu/northeastern/a6_group10/ChatActivity.java
@@ -6,43 +6,54 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private static final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private String currentUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        // get the current username from the shared preferences
+        currentUsername = getSharedPreferences(Constants.LOCAL_DATASTORE_STICKERS, MODE_PRIVATE)
+                .getString(Constants.LOCAL_DATASTORE_USERNAME_KEY, "");
+
         RecyclerView chatRecyclerView = findViewById(R.id.chatRecyclerView);
         chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Create sample dates
-        Calendar calendar = Calendar.getInstance();
-
-        calendar.set(2024, Calendar.JANUARY, 15, 10, 30);
-        Date date1 = calendar.getTime();
-
-        calendar.set(2024, Calendar.FEBRUARY, 20, 15, 45);
-        Date date2 = calendar.getTime();
-
-        calendar.set(2024, Calendar.MARCH, 5, 9, 15);
-        Date date3 = calendar.getTime();
-
-        // Populate chat list with dates and fromCount
+        // get the history from the database
         List<Chat> chatList = new ArrayList<>();
-        chatList.add(new Chat(10, date1));
-        chatList.add(new Chat(5, date2));
-        chatList.add(new Chat(12, date3));
+        String historyPath = "messages/" + currentUsername + "/history/";
+        DatabaseReference chatHistoryRef = db.getReference(historyPath);
+
+        chatHistoryRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (DataSnapshot chatSnapshot : task.getResult().getChildren()) {
+                    Chat chat = chatSnapshot.getValue(Chat.class);
+                    Log.d("ChatActivity", "Chat: " + chat);
+                    if (chat == null) {
+                        Log.e("ChatActivity", "Chat is null");
+                        continue;
+                    }
+                    chatList.add(chat);
+                }
+            } else {
+                // Handle error
+                Log.e("ChatActivity", "Error getting chat history", task.getException());
+            }
+        });
 
         // Set the adapter
         ChatRecycler adapter = new ChatRecycler(chatList);
@@ -61,7 +72,6 @@ public class ChatActivity extends AppCompatActivity {
                 startActivity(intent);
                 overridePendingTransition(0, 0);
                 return false; //so that this does not remain selected when the activity comes into focus
-
             }
             return false;
         });
